f6a01d16aebf311fffdbfab96eb7a98133720caf
monsterstudios <info@monsterstudios.com.ng>
2019-08-06 11:15:33 +0100

    Add files via upload

--- js/dashboard.js
+++ js/dashboard.js
@@ -100,13 +100,24 @@ $(function() {
                     $('.card-instagram').remove();
                 } else {
                     //Do something
-                    $('#accordionSocial').append('<div class="card card-instagram"><div class="card-header"><h2 class="mb-0"> <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne"> Instagram </button></h2></div><div id="collapseOne" class="collapse show" data-parent="#accordionSocial"><div class="card-body"><div class="form-group row my-4"><div class="col-sm-8"> <label class="col-form-label mt-3">Select the content type(s) you&#39;d like creators to make</label> <select required="" class="form-control" multiple="multiple"><option>Video</option><option>Comedic Sketch</option><option>Photograph</option><option>Music</option><option>Product / service review</option><option>Blog Post</option><option>Tweets & Retweet</option><option>Tutorial</option><option>Stories [instagram]</option> </select><div class="help-block with-errors"></div></div></div><div class="form-group row"> <label class="col-sm-8 col-form-label">Enter required number of content per platform</label><div class="col-sm-8"><div class="content-figure"></div></div></div></div></div></div>').ready(function () {
+                    $('#accordionSocial').append('<div class="card card-instagram"><div class="card-header"><h2 class="mb-0"> <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne"> Instagram </button></h2></div><div id="collapseOne" class="collapse show" data-parent="#accordionSocial"><div class="card-body"><div class="form-group row my-4"><div class="col-sm-8"> <label class="col-form-label mt-3">Select the content type(s) you&#39;d like creators to make</label> <select required="" class="form-control" multiple="multiple"><option>Video</option><option>Comedic Sketch</option><option>Photograph</option><option>Music</option><option>Product / service review</option><option>Blog Post</option><option>Tweets & Retweet</option><option>Tutorial</option><option>Stories [instagram]</option> </select><div class="help-block with-errors"></div></div></div><div class="form-group row"> <label class="col-sm-8 col-form-label">Enter required number of content per platform</label><div class="col-sm-8"><div class="content-figure"><input id="txt_1" type="hidden"></div></div></div></div></div></div>').ready(function () {
                         // enter code here
                         $('select[multiple="multiple"]').multiselect({
                             buttonClass: 'btn btn-light btn-block text-left',
                             onChange: function(option, checked, select){
-                             var selectedOptions = $('select[multiple="multiple"]').val();
-                             $('.content-figure').append('<input class="form-control my-3" type="number" placeholder="'+$(option).val()+'">');
+                             //Get all selected values   
+                             //var selectedOptions = $('select[multiple="multiple"]').val();
+                                  var lastid = $(".content-figure input:last").attr("id");
+                                  var split_id = lastid.split("_");
+                                  var nextindex = Number(split_id[1]) + 1;
+                                if ($(option).is(':selected')){
+                                    $('.content-figure').append('<input id="txt_'+ nextindex +'" class="form-control my-3" type="number" placeholder="'+$(option).val()+'">');
+                                }else{
+                                    var id = $('.content-figure input:last').attr("id");
+                                    var split_id = id.split("_");
+                                    var deleteindex = split_id[1];
+                                    $('#txt_'+deleteindex).remove();
+                                }
                             } 
                         });
                     });
